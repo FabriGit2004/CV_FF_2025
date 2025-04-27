66dd7cf39bd6a729bb6ee933abd44d6b1b45a8e4
Fabrizio Fatecha <FatechaFabrizio@hotmail.com>
2025-04-26 22:28:44 -0300

    fix try 2

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,7 +6,7 @@ const txt2 = `Desarrollo de Software.`;
 const style = { backgroundColor: "#f1502f", color: "white" }
 
 const Experience = () => (
-  <Box sx={{ my: 4 }}>
+  <Box sx={{ my: 2 }}>
     <Typography variant="h5" sx={{ fontWeight: 'bold', mb: 2 }}>Experiencia</Typography>
     <Paper sx={{ p: 3, boxShadow: 3 }}>
       <Typography variant="h6" sx={{ fontWeight: 'bold' }}><a href="https://www.itti.digital" style={{ textDecoration: 'none', color: '#2feeaf' }}>ITTI SAECA</a></Typography>
